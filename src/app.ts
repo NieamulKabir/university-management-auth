c75fd88322fb20a8272072ee4b12a266b231db74
NieamulKabir <kabir000079@gmail.com>
2023-06-26 00:04:16 +0600

    complete setup mongoose parser corse express

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
+import cors from 'cors';
+
+//use cors
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    res.send('Working Successfully')
 })
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import config from "./config/index"
 async function main() {
     try {
         await mongoose.connect(config.database_url as string);
-        console.log(`DataBase is connected successfully`);
+        console.log(`💻DataBase is connected successfully`);
 
         app.listen(config.port, () => {
             console.log(`Application listening on port ${config.port}`)
