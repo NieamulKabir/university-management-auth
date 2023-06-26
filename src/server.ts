3c5aa3805d0675bb827cf1b27c823b0611c83aa8
NieamulKabir <kabir000079@gmail.com>
2023-06-26 21:51:51 +0600

    id create

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index"
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`💻DataBase is connected successfully`);
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`💻DataBase is connected successfully`)
 
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        })
-
-    } catch (error) {
-        console.log('Failed to connect database', error);
-    }
-
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Failed to connect database', error)
+  }
 }
-main();
+main()
