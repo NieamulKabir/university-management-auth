8d4913b2a440683decb0f65e7a8d7dc3a8bb980e
NieamulKabir <kabir000079@gmail.com>
2023-07-07 00:43:23 +0600

    semester validation

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,15 @@
+import ApiError from '../../../errors/ApiError';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemesterModel';
+const status = import('http-status');
 
 const createSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError((await status).BAD_REQUEST, 'Invalid Semester Code');
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
