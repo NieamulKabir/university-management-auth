55a4b5293d106b314c8fe1360d4ee80cfa17c988
NieamulKabir <kabir000079@gmail.com>
2023-07-07 01:08:52 +0600

    try catch hoc

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,25 +1,22 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import { AcademicSemesterService } from './academicSemester.service';
+import catchAsync from '../../../shared/catchAsync';
 
-const createSemester: RequestHandler = async (req, res) => {
-  try {
+const createSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { ...academicSemesterData } = req.body;
     const result = await AcademicSemesterService.createSemester(
       academicSemesterData
     );
+    next();
     res.status(200).json({
       success: true,
       message: 'Academic Semester Created Successfully',
       data: result,
     });
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-      message: 'failed to create Academic Semester',
-    });
   }
-};
+);
 
 export const AcademicSemesterController = {
   createSemester,
